cf478fdffe61c2024ee78e7a0f1cd0668ad38752
Driton007 <59842795+Driton007@users.noreply.github.com>
2025-05-08 10:41:02 +0200

    Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import laravel from "laravel-vite-plugin";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [laravel(["resources/css/tailwind.css", "resources/js/site.js"])],
+    plugins: [
+        tailwindcss(),
+        laravel({
+            input: ["resources/css/site.css", "resources/js/site.js"],
+            refresh: true,
+        }),
+    ],
 });
